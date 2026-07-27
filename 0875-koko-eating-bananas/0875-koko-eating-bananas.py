8ce00855d3fc8b98d248ad83b009077fa5ff7ea1
Ayush <ayushmourya43@gmail.com>
2026-07-27 21:17:23 +0530

    Time: 159 ms (86.07%), Space: 20.5 MB (96.27%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,22 @@
+import math
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        
+        low = 1 
+        high = max(piles) # matlab maximum kele in one hour
+
+        while low <= high:
+
+            mid = low +(high - low)//2    #   Assume Koko ki speed = mid bananas/hour
+            hours = 0             # Is speed par total kitne hours lagenge
+            for pile in piles:
+                hours += math.ceil(pile/mid)    # Ek pile ko finish karne me kitne hours lagenge
+                
+
+            if hours <= h:    # Agar h hours ke andar finish ho gaya
+                high =mid -1  # Aur chhoti speed try karo
+
+            else:       
+                low = mid +1  # Speed kam hai, badhao
+
+        return low        # valid speed     
