610fc18187754f1aecbd200b155101e89aabce66
Ayush <ayushmourya43@gmail.com>
2026-08-02 17:39:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.59%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+
+        # String ko words ki list me convert karo
+        words = s.split()
+
+        # Length same honi chahiye
+        if len(pattern) != len(words):
+            return False
+
+        # Character -> Word
+        char_to_word = {}
+
+        # Word -> Character
+        word_to_char = {}
+
+        # Bilkul 205 ki tarah
+        for i in range(len(pattern)):
+
+            c1 = pattern[i]
+            c2 = words[i]
+
+            # Character -> Word mapping
+            if c1 in char_to_word:
+
+                if char_to_word[c1] != c2:
+                    return False
+
+            else:
+                char_to_word[c1] = c2
+
+
+            # Word -> Character mapping
+            if c2 in word_to_char:
+
+                if word_to_char[c2] != c1:
+                    return False
+
+            else:
+                word_to_char[c2] = c1
+
+        return True
