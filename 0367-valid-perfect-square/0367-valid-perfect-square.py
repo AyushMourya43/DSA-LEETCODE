ab62cef23f294a6bd0ed3b4a7984d739e58848bd
Ayush <ayushmourya43@gmail.com>
2026-09-24 16:48:02 +0530

    Time: 11 ms (7.92%), Space: 12.4 MB (19.27%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -8,3 +8,22 @@ class Solution(object):
             i +=1
 
         return False   
+
+# import math
+
+# class Solution(object):
+#     def isPerfectSquare(self, num):
+        
+#         if math.isqrt(num) ** 2 == num:
+#             return True
+#         else:
+#             return False
+
+
+
+# import math
+
+# class Solution(object):
+#     def isPerfectSquare(self, num):
+#         return math.isqrt(num) ** 2 == num
+
